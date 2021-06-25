b2a0d3a0911e185f3162df110d384ece68a6a861
Pedro Lucas <pedro.lucaswpl@gmail.com>
2021-06-25 18:03:30 -0300

    grid produtos

---
+++ _backend/_controller/_select/_grid/produto_select_grid.php
@@ -0,0 +1,10 @@
+<?php
+	require_once('../../../_class/global.php');
+	require_once('../../../_class/makeTable.php');
+	session_start();
+	$sql = 'SELECT * FROM produto';	
+
+	$table = new MakeTable($sql, $_REQUEST);
+	$table-> makeData();
+	echo json_encode($table->getResponse($fullData));
+?>
---
+++ _backend/_view/_grid/produto_grid.php
@@ -0,0 +1,44 @@
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        
+        <!-- Botões de ação -->
+        
+        <div class="row blocoBotoes">
+            <div class="col-md-12">
+                <span id="inicioBotoes"></span>
+                
+                <span id="botoesEspecificos">
+
+                </span>
+                
+                <span id="fimBotoes"></span>
+            </div>
+        </div>
+        <!-- Main row -->
+        <?php 
+            session_start();
+            include($_SESSION['GRID']);
+        ?>
+        <!-- /.row -->
+      </div><!--/. container-fluid -->
+    </section>
+    <script>
+        var colunas = [
+            ['id', 'ID'],
+            ['xProd', 'Descrição'],
+            ['NCM', 'NCM'],
+            ['valor', 'Valor'],
+            ['pesoBruto', 'Peso bruto'],
+            ['pesoLiquido', 'Peos líquido'],
+            ['dataCadastro', 'Data cadastro'],
+            ['usuarioCadastroNome', 'Usuário cadastro']
+        ];
+        
+        var tabela = new makeTable(colunas, 'produto_select_grid.php');
+        tabela.setDate(['dataCadastro']);
+        tabela.verifyBusca('<?=$_GET['busca']?>');
+        var dataTables = tabela.make();
+        
+        setBotoes('produto', 'produto', 'Cadastro produto', false, tabela);
+    </script>
